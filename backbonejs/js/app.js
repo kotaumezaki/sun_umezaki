0043178bdcac2022a81a87c2a633ed46e91cd91f
kotaumezaki <3131uzkt@gmail.com>
2020-07-07 00:03:11 +0900

    backbonejs_dotinstall10

--- backbonejs/js/app.js
+++ backbonejs/js/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-// Model
+// Model (データ)
 
 var Task = Backbone.Model.extend({
     defaults: {
@@ -31,13 +31,20 @@ task1.set({title: ''},{validate: true});
 console.log(task1.toJSON());
 
 
-// View
+// View (表示)
 
 var TaskView = Backbone.View.extend({
     tagName: 'li',
-    className: 'liClass',
-    id: 'liId',
-    template: _.template("<%- title %>"),
+    // className: 'liClass',
+    // id: 'liId',
+    events: {
+        "click .command" : "sayHello"
+    },
+    sayHello: function(){
+        alert('hello!');
+    },
+    // template: _.template("<%- title %>"),
+    template: _.template($('#task-template').html()),
     render: function(){
         var template = this.template(this.model.toJSON());
         this.$el.html(template);
@@ -49,5 +56,27 @@ var taskView = new TaskView({
 });
 console.log(taskView);
 console.log(taskView.render().el);
+$('body').append(taskView.render().el);
+
+// Collection (Modelの複数形)
+
+var Tasks = Backbone.Collection.extend({
+    model: Task
+});
+
+var tasks= new Tasks([
+    {
+        title: 'task1',
+        completed: true
+
+    },
+    {
+        title: 'task2',
+    },
+    {
+        title: 'task3',
+    },
+]);
+console.log(tasks.toJSON());
 
 })();
