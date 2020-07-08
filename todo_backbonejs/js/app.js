afa252b501fda32c84eb941e204022f947bd3994
kotaumezaki <3131uzkt@gmail.com>
2020-07-09 00:01:18 +0900

    backbonejs_dotinstall12

---
+++ todo_backbonejs/js/app.js
@@ -0,0 +1,20 @@
+(function(){
+
+var Task = Backbone.Model.extend({});
+var Tasks = Backbone.Collection.extend({ model: Task });
+
+var TaskView = Backbone.View.extend({});
+var TasksView = Backbone.View.extend([
+    {
+        title: 'task1',
+        completed: true
+    },
+    {
+        title: 'task2'
+    },
+    {
+        title: 'task3'
+    }
+]);
+
+})();
