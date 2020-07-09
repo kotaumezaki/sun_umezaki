eb8c6e1307cd73bcbe2d3a7d666cd2c8e386f9d3
kotaumezaki <3131uzkt@gmail.com>
2020-07-09 16:45:34 +0900

    backbonejs_dotinstall14

--- todo_backbonejs/js/app.js
+++ todo_backbonejs/js/app.js
@@ -1,10 +1,36 @@
 (function(){
 
-var Task = Backbone.Model.extend({});
+var Task = Backbone.Model.extend({
+    //Modelのデフォルト値を定義
+    defaults:{
+        title: 'do something',
+        completed: false
+    }
+});
 var Tasks = Backbone.Collection.extend({ model: Task });
 
-var TaskView = Backbone.View.extend({});
-var TasksView = Backbone.View.extend([
+var TaskView = Backbone.View.extend({
+    tagName: 'li', //HTML表示させたいタグ
+    template: _.template($('#task-template').html()),　 //対象となるHTMLのid属性をtemplateに設定
+    render: function(){
+        var template = this.template(this.model.toJSON());　//Model（JSON形式）を渡してtemplateメソッドを実行
+        this.$el.html(template);　//$el要素にtemplateの実行結果(HTML形式）を渡す
+        return this; 
+    }
+
+});
+var TasksView = Backbone.View.extend({
+    tagName: 'ul',
+    render: function(){
+        this.collection.each(function(task){　//collectionは複数形なのでeachで回す
+            var taskView = new TaskView({model: task});　//Viewオブジェクト（単体）の生成
+            this.$el.append(taskView.render().el);　//$el要素にオブジェクトをrender(描画)したel要素を渡す
+        }, this);
+        return this;
+    }
+});
+
+var tasks = new Tasks([
     {
         title: 'task1',
         completed: true
@@ -16,5 +42,8 @@ var TasksView = Backbone.View.extend([
         title: 'task3'
     }
 ]);
+
+var tasksView = new TasksView({collection: tasks}); //Viewオブジェクト（複数）の生成
+$('#tasks').html(tasksView.render().el); //id属性にHTML（オブジェクトをrender(描画)したel要素）を渡す
 
 })();
