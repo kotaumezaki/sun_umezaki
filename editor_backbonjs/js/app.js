da9546eedca2e1bf1376a4c423780547f228bf24
kotaumezaki <3131uzkt@gmail.com>
2020-07-16 19:54:22 +0900

    type_vol.3

---
+++ editor_backbonjs/js/app.js
@@ -0,0 +1,75 @@
+var app = app || {};
+app.model = app.model || {};
+app.collection = app.collection || {};
+app.view = app.view || {};
+
+app.model.paragraph = Backbone.Model.extend({
+    defaults:{
+        id: null,
+        type: 'text',
+        value: null
+    },
+    validate: function(attr,options){
+        switch(attr.type){
+            case 'text':
+                break;
+
+            default:
+                return '指定されてない型です';
+                break;
+        }
+        if(attr.length >= 100){
+            return '段落の文字数は100文字以下である必要があります'
+        }
+    }
+});
+
+app.collection.paragraph = Backbone.Collection.extend({
+    model: app.model.paragraph,
+    url: 'data.json',
+    // url: function(){
+    //     return app.config.apiurl + '/data.json';
+    // },
+    parse: function(res){
+        return res.result;
+    }
+});
+
+app.view.editor = Backbone.View.extend({
+    el: '#editor',
+    events: {
+        'click #add-btn': 'onAdd'
+    }, 
+    onAdd: function(){
+        console.log('onAdd');
+        var paragraph = new app.model.paragraph();
+        this.collection.add(paragraph);
+    }
+});
+
+app.view.paragraph = Backbone.View.extend({
+    tagName: 'div',
+    attributes: {
+        class: 'col'
+    },
+    template: _.template(
+        '<textarea id="<%= id %>" cols="120" rows="4"><%= text %></textarea>'
+    ),
+    render: function(){
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
+
+app.view.addparagraph = Backbone.View.extend({
+    intialize: function(){
+        this.collection.on('add',this.render,this);
+    },
+    render: function(){
+        this.collection.each(function(p){　
+            var paragraphView = new app.view.paragraph({model: p});
+            this.$el.append(paragraphView.render().el);　
+        }, this);
+        return this;
+    }
+})
